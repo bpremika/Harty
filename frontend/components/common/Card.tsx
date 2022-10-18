4131aef3bcd70a05ab0ee70085c8d96a4c1341bd
Pakin Nuntasukkasem <bruceoio@hotmail.com>
2022-10-19 01:54:22 +0700

    styledprob

---
+++ frontend/components/common/Card.tsx
@@ -0,0 +1,10 @@
+import style from  '../../styles/card.module.css';
+const Card = () => {
+    return <>
+        <div className={style.card}>
+            <h1>as</h1>
+        </div>
+    </>
+}
+
+export default Card
---
+++ frontend/pages/test.tsx
@@ -0,0 +1,14 @@
+import Card from '../components/common/Card'
+import styled from 'styled-components'
+
+
+const CardContainer = styled.div`
+    display: flex;`
+const Test = () => {
+    return <CardContainer>
+                <Card/>
+        </CardContainer>
+}
+
+export default Test
+
