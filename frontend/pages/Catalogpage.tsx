d6d028f8f63fb99fa6eab1c8b957676cef2c7758
Chawin Chitsangalert <peetwin011@hotmail.co.th>
2022-10-23 22:54:52 +0700

    CatalogwithBackend

--- frontend/pages/Catalogpage.tsx
+++ frontend/pages/Catalogpage.tsx
@@ -1,12 +1,15 @@
 import { useRouter } from "next/router"
+import  styles  from '../styles/Catalog.module.css'
 
 export default function cate(){
     const router = useRouter();
 
-    return (<div>
-        <h1>
-            {router.query.name}
-            {router.query.des}
-        </h1>
+    return (<div  style={{backgroundColor : "#16213E" ,height : "100vh" , }}>
+            <h1 className = {styles.bar}>
+                {router.query.name}
+
+            </h1>
         </div>)
 }
+
+
