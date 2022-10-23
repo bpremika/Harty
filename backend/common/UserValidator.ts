a36bd58fbdc0050151a5175a7945030db545ef80
Premika Yutinatorn <premika.yu@gmail.com>
2022-10-23 22:30:56 +0700

    feats:backend final

--- backend/common/UserValidator.ts
+++ backend/common/UserValidator.ts
@@ -3,5 +3,5 @@ import { object, string, number, date, InferType, array, bool } from 'yup';
 export const userSchema = object({
     username : string().required(),
     email : string().required(),
-    password :string().required(),
+    password :string().required().min(6),
 });
---
+++ backend/routes/party.route.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express'
+import { createParty, getManyPartyCards, getOnePartyCard, getPartyPerActivity, getPartyRoom } from '../controllers/party.controller';
+
+export const partyRouter = Router()
+partyRouter.get("/",getManyPartyCards);
+partyRouter.get("/:id",getOnePartyCard);
+partyRouter.get("/room/:id",getPartyRoom);
+partyRouter.get("/activity/:id",getPartyPerActivity);
+partyRouter.post("/",createParty);
