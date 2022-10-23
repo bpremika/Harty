eca69d6c84e63d721af4358407c7c17cfbc857f9
Premika Yutinatorn <premika.yu@gmail.com>
2022-10-24 00:01:50 +0700

    feats: join party

--- backend/routes/party.route.ts
+++ backend/routes/party.route.ts
@@ -1,9 +1,17 @@
-import { Router } from 'express'
-import { createParty, getManyPartyCards, getOnePartyCard, getPartyPerActivity, getPartyRoom } from '../controllers/party.controller';
+import { Router } from "express";
+import {
+  createParty,
+  getManyPartyCards,
+  getOnePartyCard,
+  getPartyPerActivity,
+  getPartyRoom,
+  joinParty,
+} from "../controllers/party.controller";
 
-export const partyRouter = Router()
-partyRouter.get("/",getManyPartyCards);
-partyRouter.get("/:id",getOnePartyCard);
-partyRouter.get("/room/:id",getPartyRoom);
-partyRouter.get("/activity/:id",getPartyPerActivity);
-partyRouter.post("/",createParty);
+export const partyRouter = Router();
+partyRouter.get("/", getManyPartyCards);
+partyRouter.get("/:id", getOnePartyCard);
+partyRouter.get("/room/:id", getPartyRoom);
+partyRouter.get("/activity/:id", getPartyPerActivity);
+partyRouter.post("/", createParty);
+partyRouter.post("/join/:id", joinParty);
