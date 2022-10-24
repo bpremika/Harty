ffb0f11d06383e0b2d894b1d47ef23b40f3f70e0
Premika Yutinatorn <premika.yu@gmail.com>
2022-10-24 21:49:42 +0700

    feats: add getactivitylist

--- backend/routes/party.route.ts
+++ backend/routes/party.route.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { getActivitylist } from "../controllers/other.controller";
 import {
   createParty,
   getManyPartyCards,
@@ -13,5 +14,6 @@ partyRouter.get("/", getManyPartyCards);
 partyRouter.get("/:id", getOnePartyCard);
 partyRouter.get("/room/:id", getPartyRoom);
 partyRouter.get("/activity/:id", getPartyPerActivity);
+partyRouter.get("/activitylist", getActivitylist);
 partyRouter.post("/", createParty);
 partyRouter.post("/join/:id", joinParty);
