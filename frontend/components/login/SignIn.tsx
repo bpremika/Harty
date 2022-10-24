df3dd6ad419acbd5ad2c75c957ee65cd53e45c71
Premika Yutinatorn <premika.yu@gmail.com>
2022-10-24 20:09:45 +0700

    feats : login register

--- frontend/components/login/SignIn.tsx
+++ frontend/components/login/SignIn.tsx
@@ -1,32 +1,27 @@
+import axios from 'axios';
 import React, { useCallback, useRef } from 'react';
 import styles from '../../styles/Signin.module.css'
 const SignIn = () =>{
     console.log("##Refreshing")
-    const test = {
-        username: "bolona",
-        password: "123456"
-    }
     const usernameInputElement = useRef<HTMLInputElement>(null);
     const passwordInputElement = useRef<HTMLInputElement>(null);
     const toRememberInputElement = useRef<HTMLInputElement>(null);
 
-    const formHandler = (event :React.FormEvent) =>{ 
+    const formHandler = async (event :React.FormEvent) =>{ 
         event.preventDefault()
         const data = {
             username : usernameInputElement.current?.value,
             password : passwordInputElement.current?.value,
-            toRemember : toRememberInputElement.current?.value
         }
         console.log(data)
-
-        if ((data.username == test.username) && (data.password == test.password)){
-            
-        }
+        try {
+            const body = JSON.stringify(data);
+            const res = await axios.post("https://harty.onfirebyte.xyz/login", body);
+            console.log(res.data);
+          } catch (error) {
+            console.error(error);
+          }
     }
-
-
-    
-
     return <>
         <form className={styles.boxContainer} onSubmit = {formHandler}>
             <div className={styles.inputContainer}>
