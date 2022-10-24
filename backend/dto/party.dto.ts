319fdf3618b73ed0a768b24793f47d87c9d3d34f
Premika Yutinatorn <premika.yu@gmail.com>
2022-10-24 23:55:52 +0700

    fix : delete Createpartycard

--- backend/dto/party.dto.ts
+++ backend/dto/party.dto.ts
@@ -1,72 +1,67 @@
 import { User } from "@prisma/client";
 
-export interface PartyCardDTO{
-    id : number;
-    title : string;
-    topic : string;
-    image : string;
-    info : string;
-    tag : string[];
-    numpeople : number;
-    maxpeople : number;
-    master : string;
-    time: Date;
-    isPublic:boolean;
-    date:Date;
-    starttime:Date;
-    endtime:Date;
+export interface PartyCardDTO {
+  id: number;
+  title: string;
+  topic: string;
+  image: string;
+  info: string;
+  tag: string[];
+  numpeople: number;
+  maxpeople: number;
+  master: string;
+  time: Date;
+  isPublic: boolean;
+  date: Date;
+  starttime: Date;
+  endtime: Date;
 }
 
 export interface PartiesDTO {
-    total: number
-    parties : PartyCardDTO[]
-  }
-export interface PartyRoomDTO{
-    id :number;
-    title : string;
-    topic : string;
-    image : string;
-    info : string;
-    tag : string[];
-    numpeople : number;
-    maxpeople : number;
-    master : string;
-    time: Date;
-    isPublic:boolean;
-    date: Date;
-    starttime: Date;
-    endtime:Date;
-    privateDesc:string;
-    members : string[];
+  total: number;
+  parties: PartyCardDTO[];
 }
-export interface ActivitywithID{
-    id: number;
-    topic : string;
+export interface PartyRoomDTO {
+  id: number;
+  title: string;
+  topic: string;
+  image: string;
+  info: string;
+  tag: string[];
+  numpeople: number;
+  maxpeople: number;
+  master: string;
+  time: Date;
+  isPublic: boolean;
+  date: Date;
+  starttime: Date;
+  endtime: Date;
+  privateDesc: string;
+  members: string[];
 }
-export interface CreatePartyCard{
-    topics : ActivitywithID[];
+export interface ActivitywithID {
+  id: number;
+  topic: string;
 }
 
-export interface CreateParty{
-    title : string;
-    topicID : number;
-    image : string;
-    info : string;
-    tag1 : string;
-    tag2 :string;
-    tag3 :string;
-    maxpeople : number;
-    isPublic:boolean;
-    date:Date;
-    starttime:Date;
-    endtime:Date;
-    privateDesc:string;
+export interface CreateParty {
+  title: string;
+  topicID: number;
+  image: string;
+  info: string;
+  tag1: string;
+  tag2: string;
+  tag3: string;
+  maxpeople: number;
+  isPublic: boolean;
+  date: Date;
+  starttime: Date;
+  endtime: Date;
+  privateDesc: string;
 }
 
-export interface JoinParty{
-    partyid: number;
-    userid: number;
-    username : string;
+export interface JoinParty {
+  partyid: number;
+  userid: number;
+  username: string;
 }
-
-
