3a856abf4194edb09231b15802dc9bdb170b0004
Pakin Nuntasukkasem <bruceoio@hotmail.com>
2022-10-26 19:58:20 +0700

    feats:finish profiletest

--- frontend/components/common/Profile.tsx
+++ frontend/components/common/Profile.tsx
@@ -1,9 +1,11 @@
 import axios from "axios";
 import Router, { useRouter } from "next/router";
 import sty from "../../styles/Profilecard.module.css";
+import {XMarkIcon} from '@heroicons/react/20/solid'
 interface Props {
   user: string;
   image: string;
+  close:()=>void;
 }
 const Profile = (props: Props) => {
   const router = useRouter();
@@ -15,25 +17,20 @@ const Profile = (props: Props) => {
     } catch (error) {
       console.log("error");
     } finally {
-      router.push("/login");
+      router.push("/login");    
     }
   }
   return (
-    <div className={sty.profilebox}>
+      <div className={sty.profilebox}>
+      <div className={sty.close} onClick={()=>props.close()}><XMarkIcon /></div>
       <div className={sty.imagebox}>
         <img src={props.image} className={sty.image} />
       </div>
-
       <div className={sty.username}>{props.user}</div>
-      <div
-        onClick={() => {
-          Onclick();
-        }}
-        className={sty.logout}
-      >
+
+      <div onClick={() => {Onclick(); }} className={sty.logout}>
         Logout
       </div>
-    </div>
-  );
-};
+    </div>)}
+
 export default Profile;
