d5241ccd23126e5bec2f7721708b41dbb46ca37e
Pakin Puttanukulchai <65996113+OnFireByte@users.noreply.github.com>
2022-10-26 20:45:59 +0700

    fix: session bug

--- backend/controllers/user.controller.ts
+++ backend/controllers/user.controller.ts
@@ -6,73 +6,73 @@ import { ValidationError } from "yup";
 import bcrypt from "bcrypt";
 
 export const createUser = async (req: Request, res: Response) => {
-  try {
-    console.log("hello");
-    const user: CreateUser = req.body;
-    const parsedUser = userSchema.validateSync(user);
-    console.log(parsedUser);
-    const hashpassword = await bcrypt.hash(parsedUser.password, 10);
-    const newParty = await prisma.user.create({
-      data: {
-        ...parsedUser,
-        password: hashpassword,
-      },
-    });
-    res.status(200).json({ message: "sign up successful" });
-  } catch (error) {
-    res.status(400).json({ message: (error as ValidationError).message });
-  }
+    try {
+        console.log("hello");
+        const user: CreateUser = req.body;
+        const parsedUser = userSchema.validateSync(user);
+        console.log(parsedUser);
+        const hashpassword = await bcrypt.hash(parsedUser.password, 10);
+        const newParty = await prisma.user.create({
+            data: {
+                ...parsedUser,
+                password: hashpassword,
+            },
+        });
+        res.status(200).json({ message: "sign up successful" });
+    } catch (error) {
+        res.status(400).json({ message: (error as ValidationError).message });
+    }
 };
 export const login = async (req: Request, res: Response) => {
-  try {
-    const data = req.body;
-    const parsedUser = loginSchema.validateSync(data);
-    const username = parsedUser.username;
-    const user = await prisma.user.findUnique({ where: { username } });
-    console.log(user);
-    if (user === null)
-      return res.status(404).json({ message: "don't have an account" });
-    const isPasswordValid = await bcrypt.compare(
-      req.body.password,
-      user.password
-    );
-    if (!isPasswordValid) {
-      res.status(401).json({ message: "password wrong" });
-      return;
+    try {
+        const data = req.body;
+        const parsedUser = loginSchema.validateSync(data);
+        const username = parsedUser.username;
+        const user = await prisma.user.findUnique({ where: { username } });
+        console.log(user);
+        if (user === null) return res.status(404).json({ message: "don't have an account" });
+        const isPasswordValid = await bcrypt.compare(req.body.password, user.password);
+        if (!isPasswordValid) {
+            res.status(401).json({ message: "password wrong" });
+            return;
+        }
+        const sessionData = await prisma.session.create({
+            data: { userID: user.id },
+        });
+        res.cookie("token", sessionData.id);
+        res.status(200).json({ message: "login successful" });
+    } catch (error) {
+        res.status(400).json({ message: error });
     }
-    const sessionData = await prisma.session.create({
-      data: { userID: user.id },
-    });
-    req.session.token = sessionData.id;
-    res.status(200).json({ message: "login successful" });
-  } catch (error) {
-    res.status(400).json({ message: error });
-  }
 };
 
 export const userBio = async (req: Request, res: Response) => {
-  try {
-    const session = await prisma.session.findUnique({
-      where: { id: req.session.token },
-      include: {
-        user: true,
-      },
-    });
-    if (session == null || session == undefined) {
-      res.status(401).json({ message: "session error" });
-      return;
+    try {
+        console.log(req.cookies.token);
+
+        const session = await prisma.session.findUnique({
+            where: { id: req.cookies.token },
+            include: {
+                user: true,
+            },
+        });
+        if (session == null || session == undefined) {
+            res.status(401).json({ message: "session error" });
+            return;
+        }
+        res.status(200).json(session.user.username);
+    } catch (error) {
+        console.log(error);
+
+        res.status(400).json({ message: error });
     }
-    res.status(200).json(session.user.username);
-  } catch (error) {
-    res.status(400).json({ message: error });
-  }
 };
 export const logout = async (req: Request, res: Response) => {
-  try {
-    await prisma.session.delete({ where: { id: req.session.token } });
-    req.session.token = "";
-    res.status(200).json({ message: "logout successful" });
-  } catch (error) {
-    res.status(400).json({ message: "something went wrong." });
-  }
+    try {
+        await prisma.session.delete({ where: { id: req.cookies.token } });
+        res.cookie("token", "");
+        res.status(200).json({ message: "logout successful" });
+    } catch (error) {
+        res.status(400).json({ message: "something went wrong." });
+    }
 };
