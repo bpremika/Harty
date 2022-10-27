d72018d87be4b9fa1d43e6de459558555b6cd197
Chawin Chitsangalert <peetwin011@hotmail.co.th>
2022-10-27 15:24:07 +0700

    ChatDemo

---
+++ frontend/components/common/ChatDemo.tsx
@@ -0,0 +1,8 @@
+
+
+
+
+export default function Chatboxdemo(){
+    return (<img src  ="https://cdn.discordapp.com/attachments/1031508000151707718/1035105874630414396/unknown.png" alt = "ChatDemo"/>);
+
+}
